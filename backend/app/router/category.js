d6804848a1b47d64558d6be61c47cf3fc169f847
yzn <425923407@qq.com>
2020-01-13 15:13:07 +0800

    delete backend project

--- backend/app/router/category.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = app => {
-  const { router, controller } = app;
-
-  router.get("/api/category", controller.category.read);
-  router.post("/api/category", controller.category.create);
-  router.delete("/api/category", controller.category.delete);
-};
